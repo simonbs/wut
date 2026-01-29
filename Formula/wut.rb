7f5fe087df5689dcc54278d4e533bf3fb52f227b
Simon Støvring <mail@simonbs.dk>
2026-01-29 16:45:48 +0100

    Update formula for v0.1.1

--- Formula/wut.rb
+++ Formula/wut.rb
@@ -1,8 +1,8 @@
 class Wut < Formula
   desc "Git worktree manager that keeps worktrees out of your repo"
   homepage "https://github.com/simonbs/wut"
-  url "https://github.com/simonbs/wut/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "ccf538f5a8740b3236d876452e712eb587a16db51f19dfa687e1b987c5420662"
+  url "https://github.com/simonbs/wut/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "582f06836f28dfe7a621ea24cac4d277709c139abf5a50717839c369f16d267a"
   head "https://github.com/simonbs/wut.git", branch: "main"
 
   depends_on "go" => :build
