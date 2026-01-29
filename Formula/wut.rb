e2acd6a98fa49b31b594a6810a8e85e4ef98d6fd
Simon Støvring <mail@simonbs.dk>
2026-01-29 15:58:58 +0100

    Add head for --HEAD installs

--- Formula/wut.rb
+++ Formula/wut.rb
@@ -3,6 +3,7 @@ class Wut < Formula
   homepage "https://github.com/simonbs/wut"
   url "https://github.com/simonbs/wut/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "7f01da8bfa34bb390cf5c2ed8d962df3f958cf78b3b8bef91fbb23926185e469"
+  head "https://github.com/simonbs/wut.git", branch: "main"
 
   depends_on "go" => :build
 
