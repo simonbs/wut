c69d379a79a83a9dac2ef6d769b920fa531bb6c6
Simon Støvring <mail@simonbs.dk>
2026-01-29 14:05:20 +0100

    Update SHA256 for v0.1.0

--- Formula/wut.rb
+++ Formula/wut.rb
@@ -2,7 +2,7 @@ class Wut < Formula
   desc "Git worktree manager that keeps worktrees out of your repo"
   homepage "https://github.com/simonbs/wut"
   url "https://github.com/simonbs/wut/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "88a67f61a59bb0d7733ba2328b0b0a575a821330389171838a371f26dd6a7809"
+  sha256 "7f01da8bfa34bb390cf5c2ed8d962df3f958cf78b3b8bef91fbb23926185e469"
 
   depends_on "go" => :build
 
